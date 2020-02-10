13a597b781a210703355fddda0111ad7e98e65c3
ChimGoKien <38057360+quocle108@users.noreply.github.com>
2020-02-10 16:09:00 +0700

    Update cannet.r

--- cannet.r
+++ cannet.r
@@ -2,7 +2,7 @@ class Cannet < Formula
 
    homepage "https://github.com/canfoundation/CAN"
    revision 0
-   url "https://github.com/canfoundation/CAN/releases/download/can-v1.0.0/cannet-1.0.0.mojave.bottle.tar.gz"
+   url "https://github.com/canfoundation/can/archive/can-v1.0.0.tar.gz"
    version "1.0.0"
 
    option :universal
@@ -15,8 +15,8 @@ class Cannet < Formula
    depends_on :arch =>  :intel
 
    bottle do
-      root_url "https://github.com/canfoundation/CAN/releases/download/can-v1.0.0"
-      sha256 "c29d77f9a76c1ccbdd5d27df5d6882162cdf03166cebf8c0f05d5520c50d6a37" => :mojave
+      root_url "https://github.com/canfoundation/can/releases/download/v2.0.2"
+      sha256 "f3a36a6b43a434a7c4eaf4501c2c52975f47f15ecdb96390277ac37e5aafafb0" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
